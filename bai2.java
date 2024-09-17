a0dbe32c30d48802a355f4460abef49ef223542d
Myduyen495 <dmy61151@gmail.com>
2024-09-17 10:15:37 +0700

    Add files via upload

--- bai2.java
+++ bai2.java
@@ -1,15 +1,22 @@
-public class bai2 {
-    public static void main(String[] args) {
-        // Khai báo các biến
-        String name = "Quý Bửu"; // Tên (kiểu String)
-        int age = 18;             // Tuổi (kiểu int)
-        double height = 1.6;     // Chiều cao (kiểu double, ví dụ: 1.75 mét)
-        boolean likesProgramming = true; // Có thích lập trình không (kiểu boolean)
+package tuan2;
 
-        // In ra các giá trị đã lưu
-        System.out.println("Tên: " + name);
-        System.out.println("Tuổi: " + age);
-        System.out.println("Chiều cao: " + height + " mét");
-        System.out.println("Có thích lập trình không: " + (likesProgramming ? "Có" : "Không"));
+import java.util.Scanner;
+public class bai2{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("nhap vao ho va ten ");
+        String ten = sc.nextLine();
+        System.out.print("nhap vao tuoi ");
+        int tuoi = sc.nextInt();
+        System.out.print("nhap vao chieu cao ");
+        double ccao = sc.nextDouble();
+        System.out.print("ban co thich lap trinh k ");
+        boolean ltrinh = sc.nextBoolean();
+        
+        System.out.println("..............");
+        System.out.println(ten);
+        System.out.println(tuoi);
+        System.out.println(ccao);
+        System.out.println(ltrinh);
     }
 }

aae4909a0b601c5140f463a81ef3e721172f4e7a
Myduyen495 <dmy61151@gmail.com>
2024-09-17 09:51:12 +0700

    Add files via upload

--- bai1.java
+++ bai1.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
+package tuan2;
 
-public class bai1 {
-    public static void main(String[] args) {
-        // Tạo đối tượng Scanner để đọc dữ liệu từ bàn phím
-        Scanner scanner = new Scanner(System.in);
-        
-        // Hiển thị thông báo yêu cầu người dùng nhập tên
-        System.out.print("Nhập tên của bạn: ");
-        
-        // Đọc tên người dùng từ bàn phím
-        String name = scanner.nextLine();
-        
-        // Hiển thị lời chào với tên người dùng
-        System.out.println("Hello " + name);
-        
-        // Đóng đối tượng Scanner
-        scanner.close();
+import java.util.Scanner;
+public class bai1
+{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("nhap ten cua ban: ");
+        String ten= sc.nextLine();
+        System.out.println("Hello "+ten);
     }
 }
